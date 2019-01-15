5d6d4d79fbba29e3cc08ae81a88a2a66d47f92ff
pavelmakarets <makaretsp@mail.ru>
2019-01-15 13:04:31 +0200

    framework

--- NunitTestFramework/NunitTestFramework/Businnes Objects/PassengerInfo.cs
+++ NunitTestFramework/NunitTestFramework/Businnes Objects/PassengerInfo.cs
@@ -7,26 +7,47 @@ namespace NunitTestFramework.Businnes_Objects
     /// </summary>
     public class PassengerInfo
     {
-        public string Title { get; set; } = "Dr";
-        public string Firstname { get; set; } = "James";
-        public string Lastname { get; set; } = "Wilson";
-        public DateTime DateOfBirth { get; set; } = new DateTime(DateTime.Now.Year - 25, 09, 29);
-        public string Gender { get; set; } = "Male";
-        public string Email { get; set; } = "JamesWilson@email.com";
-        public string PhoneType { get; set; } = "Mobile";
-        public string PhoneNumberLocal { get; set; } = "United States (1)";
-        public string PhoneContactNumber { get; set; } = "2345678900";
-        public string NumberCard { get; set; } = "5345123111123453";
-        public string FirstnameCard { get; set; } = "James";
-        public string LastnameCard { get; set; } = "Wilson";
-        public DateTime ExpiryDateCard { get; set; } = new DateTime(DateTime.Now.Year + 3, 09, 29);
-        public string SecurityCodeCard { get; set; } = "123";
-        public string Country { get; set; } = "United States";
-        public string HouseNameOrNumber { get; set; } = "11301";
-        public string ZipCode { get; set; } = "90064";
-        public string FullAdress { get; set; } = "#1901, 10390 Wilshire Blvd Apt 1, Los Angeles CA 90024-6400";
+        public string Title { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneType { get; set; }
+        public string PhoneNumberLocal { get; set; }
+        public string PhoneContactNumber { get; set; }
+        public string NumberCard { get; set; }
+        public string FirstnameCard { get; set; }
+        public string LastnameCard { get; set; }
+        public DateTime ExpiryDateCard { get; set; }
+        public string SecurityCodeCard { get; set; }
+        public string Country { get; set; }
+        public string HouseNameOrNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string FullAdress { get; set; }
 
-        public PassengerInfo() { }
+        public PassengerInfo()
+		{
+			Title = "Dr";
+			Firstname = "James";
+			Lastname = "Wilson";
+			DateOfBirth = new DateTime(DateTime.Now.Year - 25, 09, 29);
+			Gender = "Male";
+			Email = "JamesWilson@email.com";
+			PhoneType = "Mobile";
+			PhoneNumberLocal = "United States (1)";
+			PhoneContactNumber = "2345678900";
+			NumberCard = "5345123111123453";
+			FirstnameCard = "James";
+			LastnameCard = "Wilson";
+			ExpiryDateCard = new DateTime(DateTime.Now.Year + 3, 09, 29);
+			SecurityCodeCard = "123";
+			Country = "United States";
+			HouseNameOrNumber = "11301";
+			ZipCode = "90064";
+			FullAdress = "#1901, 10390 Wilshire Blvd Apt 1, Los Angeles CA 90024-6400";
+		}
+
         public PassengerInfo(string title, string firstname, string lastname, DateTime dateOfBirth, string gender,
             string email, string phoneType, string numberLocal, string contactNumber,
             string cardNumber, string firstnameCard, string lastnameCard, DateTime expiryDateCard, string securityCode,
